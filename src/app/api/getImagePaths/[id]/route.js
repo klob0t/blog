ad57cc04a994eea22bbe23aca6d7afbedf6ce4b5
klob0t <klob0t@yahoo.com>
2024-03-24 09:34:11 +0700

    lets try vercel

--- src/app/api/getImagePaths/[id]/route.js
+++ src/app/api/getImagePaths/[id]/route.js
@@ -1,6 +1,13 @@
 import fs from 'fs';
 import path from 'path';
 
+export async function generateStaticParams(){
+  const directoryPath = path.join(process.cwd(), 'public', 'carousel');
+  const ids = fs.readdirSync(directoryPath);
+  const paths = ids.map (id => ({params: {id}}))
+  return paths
+}
+
 export async function GET(request, context) {
   const id = context.params.id
   const directoryPath = path.join(process.cwd(), 'public', 'carousel', `${id}`);
