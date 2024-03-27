f985e89c7f43e4ed8046a3a1c24449c48fe92db9
klob0t <klob0t@yahoo.com>
2024-03-27 14:53:05 +0700

    added proper title to blog pages

--- src/app/blogs/[slug]/layout.jsx
+++ src/app/blogs/[slug]/layout.jsx
@@ -1,8 +1,9 @@
-'use client'
+"use client";
 import { useEffect, useState } from "react";
+import matter from "gray-matter";
 
-export function getPostContent(slug){
-  const [content, setContent] = useState("")
+export function getPostContent(slug) {
+  const [content, setContent] = useState("");
   useEffect(() => {
     fetch(`/api/getPostContent/${slug}`)
       .then((response) => {
@@ -18,17 +19,22 @@ export function getPostContent(slug){
         console.error("There was a problem with the fetch operation:", error);
       });
   }, []);
-  return content
+  return content;
 }
 
-const RootLayout = ({ children }) => {
+const RootLayout = ({ children, params }) => {
+  const slug = params.slug;
+  const data = getPostContent(slug);
+  const title = matter(data);
+  const complete = `klob0t blog - ${title.data.title}`;
   return (
     <html lang="en">
-      <body>
-      {children}
-      </body>
+      <head>
+        <title>{complete}</title>
+      </head>
+      <body>{children}</body>
     </html>
   );
-}
+};
 
-export default RootLayout
+export default RootLayout;
